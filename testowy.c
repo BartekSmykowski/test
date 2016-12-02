af48915b6ca9db8cbd6c65c86141c4eca97d2398
janusz96 <bsmykowski@wp.pl>
2016-12-02 20:08:48 +0100

    Including file
    File with main

---
+++ testowy.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+
+int main() {
+	int liczba_testow = 0;
+	scanf("%d", &liczba_testow);
+
+	int min = 0;
+	int max = 0;
+
+
+
+	for (; liczba_testow > 0; liczba_testow--) {
+		scanf("%d %d", &min, &max);
+
+
+
+	
+	}
+
+
+	return 0;
+}
